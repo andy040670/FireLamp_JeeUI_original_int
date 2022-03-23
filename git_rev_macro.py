e1d4c8dc240d698480ddee18dd7bf856748348f5
andy040670 <andy040670@yandex.ru>
2022-03-23 18:04:22 +0300

    Delete git_rev_macro.py

--- git_rev_macro.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-try:
-	revision = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
-	print ("-DPIO_SRC_REV=\\\"%s\\\"" % revision)
-except:
-	print ("-DPIO_SRC_REV=\\\"UNKNOWN\\\"")
