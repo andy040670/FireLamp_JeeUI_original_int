827dbf6651c98768fabc589cb31e18c7087ddece
andy040670 <andy040670@yandex.ru>
2023-02-02 20:36:54 +0300

    Update update-DEV-from-Git.cmd

--- update-DEV-from-Git.cmd
+++ update-DEV-from-Git.cmd
@@ -7,12 +7,10 @@ cls
 Echo.
 
 if "%param%"=="1" ( rem переключаем на мастер
-        "%ProgramFiles%\Git\bin\git" config --global --add safe.directory D:/FireLamp_JeeUI_original_int
 	"%ProgramFiles%\Git\bin\git" checkout master
 	"%ProgramFiles%\Git\bin\git" pull
 )
 if "%param%"=="2" ( rem переключаем на дев
-        "%ProgramFiles%\Git\bin\git" config --global --add safe.directory D:/FireLamp_JeeUI_original_int
 	"%ProgramFiles%\Git\bin\git" checkout dev
 	"%ProgramFiles%\Git\bin\git" pull
 )
